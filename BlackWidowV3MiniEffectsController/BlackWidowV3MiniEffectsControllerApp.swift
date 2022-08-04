d8b6f9257a97b0b7398416674b86727500c048c2
Jiqi Sun <sunqqxp@gmail.com>
2022-08-04 14:38:43 +0100

    Initial Commit

---
+++ BlackWidowV3MiniEffectsController/BlackWidowV3MiniEffectsControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlackWidowV3MiniEffectsControllerApp.swift
+//  BlackWidowV3MiniEffectsController
+//
+//  Created by Jiqi Sun on 04/08/2022.
+//
+
+import SwiftUI
+
+@main
+struct BlackWidowV3MiniEffectsControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
